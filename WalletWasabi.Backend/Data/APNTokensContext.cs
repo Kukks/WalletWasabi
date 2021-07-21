7106780dce89d2dd712238d003305724f70be112
DanGould <d@ngould.dev>
2021-07-20 23:07:21 -0400

    control token db

--- WalletWasabi.Backend/Data/APNTokensContext.cs
+++ WalletWasabi.Backend/Data/APNTokensContext.cs
@@ -7,16 +7,6 @@ namespace WalletWasabi.Backend.Data
 	{
 		public DbSet<AppleDeviceToken> Tokens { get; set; }
 
-		//public APNTokensContext()
-		//{
-
-		//}
-
-		//public APNTokensContext(DbContextOptions<APNTokensContext> options) : base(options)
-		//{
-
-		//}
-
 		protected override void OnConfiguring (DbContextOptionsBuilder builder)
 		{
 			builder.UseNpgsql("Host=localhost;Database=postgres;Username=dan;Password=;");
