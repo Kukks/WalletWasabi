ec19a60a5dffbced46ad48a41ec107a1a6736946
DanGould <d@ngould.dev>
2021-07-26 15:08:56 -0400

    send first successful push from backend had to rm Task.WhenAll on tokens => SendNotification because it wasn't creating tasks and DbContext is not thread safe

--- WalletWasabi.Backend/Global.cs
+++ WalletWasabi.Backend/Global.cs
@@ -93,7 +93,7 @@ namespace WalletWasabi.Backend
 			var indexFilePath = Path.Combine(indexBuilderServiceDir, $"Index{RpcClient.Network}.dat");
 			var blockNotifier = HostedServices.FirstOrDefault<BlockNotifier>();
 			IndexBuilderService = new IndexBuilderService(RpcClient, blockNotifier, indexFilePath);
-			Coordinator = new Coordinator(RpcClient.Network, blockNotifier, Path.Combine(DataDir, "CcjCoordinator"), RpcClient, roundConfig, () => _ = serviceProvider.GetRequiredService<SendPushService>().SendNotificationsAsync(false));
+			Coordinator = new Coordinator(RpcClient.Network, blockNotifier, Path.Combine(DataDir, "CcjCoordinator"), RpcClient, roundConfig, () => _ = serviceProvider.GetRequiredService<SendPushService>().SendNotificationsAsync(true));
 			IndexBuilderService.Synchronize();
 			Logger.LogInfo($"{nameof(IndexBuilderService)} is successfully initialized and started synchronization.");
 
