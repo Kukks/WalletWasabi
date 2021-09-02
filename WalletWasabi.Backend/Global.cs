57317b233a158301d3e0fa3c5a48e3d897611697
DanGould <d@ngould.dev>
2021-09-02 11:59:41 -0400

    configure notifications isDebug @ coordinator

--- WalletWasabi.Backend/Global.cs
+++ WalletWasabi.Backend/Global.cs
@@ -89,7 +89,8 @@ namespace WalletWasabi.Backend
 			var indexFilePath = Path.Combine(indexBuilderServiceDir, $"Index{RpcClient.Network}.dat");
 			var blockNotifier = HostedServices.FirstOrDefault<BlockNotifier>();
 			IndexBuilderService = new IndexBuilderService(RpcClient, blockNotifier, indexFilePath);
-			Coordinator = new Coordinator(RpcClient.Network, blockNotifier, Path.Combine(DataDir, "CcjCoordinator"), RpcClient, roundConfig, () => _ = serviceProvider.GetRequiredService<SendPushService>().SendNotificationsAsync(false));
+			Coordinator = new Coordinator(RpcClient.Network, blockNotifier, Path.Combine(DataDir, "CcjCoordinator"), RpcClient, roundConfig,
+				() => _ = serviceProvider.GetRequiredService<SendPushService>().SendNotificationsAsync(roundConfig.IsDebug));
 			IndexBuilderService.Synchronize();
 			Logger.LogInfo($"{nameof(IndexBuilderService)} is successfully initialized and started synchronization.");
 
